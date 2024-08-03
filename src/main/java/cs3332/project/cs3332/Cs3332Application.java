0d106fdfb54a5a91d34f9d6eb63f21f768a2fcc1
SimonTranDuy <duycucai2014@gmail.com>
2024-08-04 00:56:21 +0700

    first commit

---
+++ src/main/java/cs3332/project/cs3332/Cs3332Application.java
@@ -0,0 +1,13 @@
+package cs3332.project.cs3332;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs3332Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs3332Application.class, args);
+	}
+
+}
---
+++ src/test/java/cs3332/project/cs3332/Cs3332ApplicationTests.java
@@ -0,0 +1,13 @@
+package cs3332.project.cs3332;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs3332ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
